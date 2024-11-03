b4d40425af812e58b828f6b4775b952978201c58
OndaBaby <139729721+OndaBaby@users.noreply.github.com>
2024-11-03 15:27:31 +0800

    draft/flask-backend/react-frontend

--- server/server.py
+++ server/server.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/members')
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+@app.route('/')
+
+def server():
+    return ("connected")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
